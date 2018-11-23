104dde7faa5c2f522e93c0ecc48b7555a577ae2f
Mantas Stankevicius <mantydze@gmail.com>
2018-11-23 15:46:15 +0100

    added example how to use client from p5 network

---
+++ examples/08-p5-network.py
@@ -0,0 +1,17 @@
+""" Simple example.
+    Fetch list of eras
+"""
+
+from __future__ import print_function
+from omsapi import OMSAPI
+
+omsapi = OMSAPI("https://cmsoms.cms./agg/api", "v1")
+
+# Create a query.
+q = omsapi.query("eras")
+
+# Execute query & fetch data
+response = q.data()
+
+# Display JSON data
+print(response.json())
