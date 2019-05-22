a46bbd2b387f1f8cdf37807811034a107608c651
Remi <remigius.mommsen@cern.ch>
2019-05-22 16:54:37 +0200

    Change naming to include python version and add hack to encode dependency into RPM

--- setup.py
+++ setup.py
@@ -1,4 +1,24 @@
 import setuptools
+import sys
+
+if sys.version_info.major == 2:
+    python_version = 'python2-'
+else:
+    python_version = 'python'+str(sys.version_info.major)+str(sys.version_info.minor)+'-'
+
+RPM_REQUIRED_DEPS = python_version+'requests'
+
+## HACK FOR DEPS IN RPMS (taken from https://gist.github.com/primalmotion/1561092)
+from setuptools.command.bdist_rpm import bdist_rpm
+def custom_make_spec_file(self):
+    spec = self._original_make_spec_file()
+    lineDescription = "%description"
+    spec.insert(spec.index(lineDescription) - 1, "requires: %s" % RPM_REQUIRED_DEPS)
+    return spec
+bdist_rpm._original_make_spec_file = bdist_rpm._make_spec_file
+bdist_rpm._make_spec_file = custom_make_spec_file
+## END OF HACK
+
 
 with open("README.md", "r") as fh:
 
@@ -6,7 +26,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 
-    name='omsapi',
+    name=python_version+'omsapi',
 
     version='0.5.0',
 
