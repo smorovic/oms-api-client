ca978afbdf4a4495356050f6a586507848167aba
Srecko Morovic <smorovic@cms-oms-build-01.cern.ch>
2023-06-14 17:51:36 +0200

    add pypi publish script. Token needs to be set separately

---
+++ build_pypi.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+PYVER=`/usr/bin/python3 -c 'import sys; print(".".join(map(str, sys.version_info[:2])))'`
+
+rm -rf venv
+
+if [ $PYVER == "3.8" ]; then
+  #RHEL8 and python3.8
+  python3.8 -m venv venv
+else
+  virtualenv-3 venv
+fi
+source venv/bin/activate
+pip3 install --upgrade pip
+pip3 install --upgrade setuptools
+pip3 install --upgrade twine
+pip3 install --upgrade wheel
+
+rm -rf dist/*.whl
+python3 setup.py bdist_wheel
+wheelfile=`ls dist/*.whl -t | head -n 1`
+echo "wheel file: ${wheelfile}"
+python3 -m twine upload --verbose --repository gitlab ${wheelfile}
+
+rm -rf venv
